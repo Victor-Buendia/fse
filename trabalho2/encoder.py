ff475a2c1da62935d93d2a5055c2447f8965c091
Victor Buendia <victorbuendia03@gmail.com>
2023-06-17 16:47:17 -0300

    update

--- trabalho2/encoder.py
+++
@@ -1,9 +0,0 @@
-# uart = serial.Serial('/dev/serial0')
-endereco = 1
-codigo = 22
-subcodigo = 211
-matricula = 2386
-codigo2 = 1
-mensagem = ([endereco, codigo, subcodigo] + [int(digito) for digito in str(matricula)] + [codigo2])
-
-print(mensagem)
--- trabalho2/main.py
+++
@@ -1,5 +0,0 @@
-from program import Program
-
-if __name__ == "__main__":
-	program = Program()
-	program.start()
